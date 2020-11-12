a5141d83c587fccf6a07d8b2f9d406481118d0ce
Madhu Krishna Dasari <madhu.krish321@gmail.com>
2020-11-12 12:34:54 +0000

    Made changes in MenuItem to naviage using withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
 import './App.css'
 
+const HatsPage = () => (
+  <h3>
+    This is Hats Shopping Page
+  </h3>
+)
+
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop/hats' component={HatsPage} />
+    </Switch>
   );
 }
 
