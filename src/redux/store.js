f01b5d30b209338c18b516ad5f2e94ca547af4a5
Madhu Krishna Dasari <madhu.krish321@gmail.com>
2020-11-19 15:58:55 +0000

    Added CartIcon and CartDropdown components to our App and also implemented cartReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import userReducer from './user/user.reducer'
+import cartReducer from './cart/cart.reducer'
 
 const middleWares = [logger]
 
 const rootReducer = combineReducers({
-  user: userReducer
+  user: userReducer,
+  cart: cartReducer
 })
 
 const store = createStore(rootReducer, applyMiddleware(...middleWares))
